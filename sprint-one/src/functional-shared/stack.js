8d7bbfac31cddf1831393c8a88b750bcd69057aa
css1990x <css1990x>
2017-10-11 18:25:17 -0700

    second commit

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.lastIdx = 0;
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Implement the methods below
+  push : function(value) {
+    var maxIdx = this.lastIdx; 
+    this.storage[maxIdx] = value; 
+    this.lastIdx++; 
+  },
 
+  pop : function() {
+    if (this.lastIdx > 0) {    
+      var maxIdx = Object.keys(this.storage).length - 1;
+      var lastVal = this.storage[maxIdx];
+      delete this.storage[maxIdx];
+      this.lastIdx--;
+      return lastVal;
+    }
+    return;
+  },
+
+  size : function() {
+    return this.lastIdx; 
+  }
+};
 
