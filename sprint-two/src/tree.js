5ce4094c2d929d16ea8c76cbee7e42ab0fa9d679
css1990x <css1990x>
2017-10-12 16:15:19 -0700

    Implement Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,38 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods); 
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  if (this.value === null) {
+    this.value = value;
+  } else {
+    var child = Tree(value); 
+    this.children.push(child);
+  } 
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+
+  var checkTree = function(target, tree) {
+    if (tree.value === target) {
+      result = true;
+    }
+    var hasChildren = tree.children.length > 0;
+    if (hasChildren) {
+      for (var i = 0; i < tree.children.length; i++) {
+        checkTree(target, tree.children[i]);
+      }
+    }
+  };
+  checkTree(target, this);
+  return result;
 };
 
 
