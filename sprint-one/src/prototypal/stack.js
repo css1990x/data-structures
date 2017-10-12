d90570dd1652558ee6e78ebd67ec5f006a672063
css1990x <css1990x>
2017-10-12 10:23:41 -0700

    Implement Stack using prototypal inheritence

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {}; 
+  someInstance.counter = 0; 
+  return someInstance; 
 };
 
-var stackMethods = {};
+stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.counter] = value; 
+  this.counter++; 
+};
+
+stackMethods.size = function() {
+  return this.counter; 
+};
+
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    var maxidx = this.counter - 1; 
+    var lastval = this.storage[maxidx]; 
+    delete this.storage[maxidx];  
+    this.counter--; 
+    return lastval; 
+  }
+};
 
