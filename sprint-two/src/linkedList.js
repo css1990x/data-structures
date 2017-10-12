e5cd8651b259a3dcb0c119daa61f9cbb8c7ffe8a
css1990x <css1990x>
2017-10-12 15:30:28 -0700

    finished linkedlist challenge

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,57 @@
+// var list  {
+//   head: { value: 4 };
+//   tail: { value: 5 };
+//   addToTail: function,
+//   removeHead: function,
+//   contains: function,
+
+//   4 : {
+//     value: 4
+//     next: 5
+//   },
+//   5 : {
+//     value: 5,
+//     next: null
+//   }
+// }
+
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    // if list is empty...
+    if (list.head === null) {
+      list.head = {};
+      list.tail = {}; 
+      // Assign value to list.head
+      list.head.value = value;
+      // Assign value to list.tail
+      list.tail.value = value;
+      _.extend(list, Node(value));
+    } else {
+    // else...
+      // Store old list tail
+      var oldTailVal = list.tail.value; 
+      // Add new tail node
+      _.extend(list, Node(value));
+      list.tail.value = value;
+      // Assign new value as old tail's reference
+      list[oldTailVal].next = value;
+    }
   };
 
   list.removeHead = function() {
+    var currentHead = list.head.value; 
+    list.head.value = list[currentHead].next;  
+    delete list[currentHead];
+    return currentHead; 
   };
 
   list.contains = function(target) {
+    return (list[target] !== undefined);
   };
 
   return list;
@@ -18,8 +60,9 @@ var LinkedList = function() {
 var Node = function(value) {
   var node = {};
 
-  node.value = value;
-  node.next = null;
+  node[value] = {};
+  node[value].value = value; 
+  node[value].next = null;
 
   return node;
 };
