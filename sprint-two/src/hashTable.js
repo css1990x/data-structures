02391018186f929d21d49fa56e34cdb808f0f6e3
css1990x <css1990x>
2017-10-13 14:50:32 -0700

    finished basic hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,62 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Retrive the bucket at hash key
+  var bucket = this._storage.get(index); 
+  if (!bucket) {
+    bucket = [];  
+  }
+  // If the same key already exists in the bucke
+  var includes = false; 
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+  // Replace the value
+      bucket[i][1] = v; 
+      includes = true; 
+    }    
+  }
+  // Else
+  // Push the new tuple to the bucket 
+  if (!includes) {
+    bucket.push([k, v]); 
+  }
+
+  this._storage.set(index, bucket); 
+  console.log(index);
+  console.log(this._storage.get(3));
 };
 
 HashTable.prototype.retrieve = function(k) {
+// debugger;
+  // Get key in storage array
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index); 
+  if (!bucket) {
+    return bucket;  
+  }
+  // Loop through bucket array (for loop)
+  for (var i = 0; i < bucket.length; i++) {
+    // If tuple has key 
+    if (bucket[i][0] === k) {
+      // return value. 
+      return bucket[i][1];
+    }
+  } 
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  // Loop through bucket array (for loop)
+  for (var i = 0; i < bucket.length; i++) {
+    // If tuple has key 
+    if (bucket[i][0] === k) {
+      // delete tuple 
+      bucket.splice(i, 1);
+    } 
+  }
+  return; 
 };
 
 
